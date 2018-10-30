1412c8b780703997dc1f6f78a498ad2c172ae46b
Lorenzo Borje <lorenzo.borje@gmail.com>
2018-10-31 00:55:12 +0800

    added PUT endpoint for /recipes

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.post('/recipes', jsonParser, (req, res) => {
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
+      const message = `Missing \`${field}\` in request body`;
       console.error(message);
       return res.status(400).send(message);
     }
@@ -110,6 +110,33 @@ app.delete('/recipes/:id', (req, res) => {
   console.log(`Deleted recipe \`${req.params.ID}\``);
   res.status(204).end();
 });
+
+app.put('/recipes/:id', jsonParser, (req, res) => {
+  const requiredFields = ['name', 'ingredients'];
+  
+  requiredFields.forEach(field => {
+    if (!(field in req.body)) {
+      const message = `Missing ${field} in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  });
+  
+  if (!(req.params.id === req.body.id)) {
+    const message = `Request path id ${req.params.id} must match request body id ${req.body.id}`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+
+  console.log(`Updating recipe ${req.params.id}`);
+  Recipes.update({
+    id: req.body.id,
+    name: req.body.name,
+    ingredients: req.body.ingredients
+  });
+  res.status(204).end();
+});
+
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
